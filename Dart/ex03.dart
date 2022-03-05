b81d15bc88fd2b5ee1e5f66ba3e25b3319e182a2
André Luiz <andre.silva@topi.io>
2022-03-05 10:31:12 -0300

    Exercicio 3

---
+++ Dart/ex03.dart
@@ -0,0 +1,8 @@
+void main() {
+  int celsiusTemperature = 26;
+  double farenheitTemperature = ((celsiusTemperature) * 9 / 5) + 32;
+
+  print("$celsiusTemperature °C é igual a $farenheitTemperature °F");
+  print(
+      "Formula: ($celsiusTemperature °C × 9/5) + 32 = $farenheitTemperature °F");
+}
